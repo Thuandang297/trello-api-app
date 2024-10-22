85ad0d89647343ea8d83285f1a7ec8b127956a7a
Thuandang297 <Thuandang729@gmail.com>
2024-10-23 00:09:00 +0700

    test count contributes

--- src/server.js
+++ src/server.js
@@ -24,5 +24,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
 })
