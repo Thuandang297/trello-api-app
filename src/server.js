4d4bacbe038facad18b37327414dff29f2dc5bde
Thuandang297 <Thuandang729@gmail.com>
2024-10-29 00:01:21 +0700

    install :mongo db

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-console */
 
 import express from 'express'
-const app = express()
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
+const START_SERVER = () => {
+  const app = express()
+  const hostname = 'localhost'
+  const port = 8017
+  console.log('3.Start server...')
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-const hostname = 'localhost'
-const port = 8017
+  app.listen(port, hostname, () => {
+    console.log(`This app is running at hostname:${hostname} and port:${port}`)
+  })
+}
 
-app.get('/', (req, res) => {
-  res.end('<h1>Hello World!</h1><hr>')
-})
-
-app.listen(port, hostname, () => {
-})
+(async () => {
+  try {
+    console.log('1.Connecting to mongodb...')
+    await CONNECT_DB()
+    console.log('2.Connecting to mongodb success!')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
