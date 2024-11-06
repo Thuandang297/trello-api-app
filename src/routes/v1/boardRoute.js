74bb0d361518d792fe51152b6956cf0e5fa5ff55
Thuandang297 <Thuandang729@gmail.com>
2024-11-06 12:44:32 +0700

    add :controller

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { boardValidation } from '~/validations/boardValidation'
+import { boardController } from '~/controllers/boardController'
 const boardRoute = express.Router()
 
 boardRoute.route('/')
-  .post(boardValidation.createNew)
-  .put(boardValidation.updateData)
+  .post(boardValidation.createNew, boardController.createNew)
+  .put(boardValidation.updateData, boardController.updateData)
 
 export const BoardsRouter = boardRoute
