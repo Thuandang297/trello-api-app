9f07924e386ab0fce8183c04ef3e716907cfcdb7
Thuandang297 <Thuandang729@gamil.com>
2025-03-02 18:22:27 +0700

    update cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const WHITE_LIST_DOMAINS = ['http://localhost:5173', 'http://localhost:5000']
+export const WHITE_LIST_DOMAINS = []
+// export const WHITE_LIST_DOMAINS = ['http://localhost:5173', 'http://localhost:5000']
 
 export const TYPE_BOARDS={
   PUBLIC:'public',
