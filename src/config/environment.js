9a3c8af577995710436a34e70b87b02742335a14
Thuandang297 <Thuandang729@gamil.com>
2025-03-02 17:31:59 +0700

    # This is a combination of 4 commits. # This is the 1st commit message:
    update lock
    
    # This is the commit message #2:
    
    config: appPort appHost for deploy
    
    # This is the commit message #3:
    
    update:port in config
    
    # This is the commit message #4:
    
    update config

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,9 @@
 import 'dotenv/config'
 export const env = {
-  APP_HOST: process.env.APP_HOST,
-  APP_PORT: process.env.APP_PORT,
+  APP_HOST_LOCAL: process.env.APP_HOST_LOCAL,
+  APP_PORT_LOCAL: process.env.APP_PORT_LOCAL,
+  APP_HOST_PRODUCT: process.env.HOST,
+  APP_PORT_PRODUCT: process.env.PORT,
   AUTHOR: process.env.AUTHOR,
   MONGO_DB_URI: process.env.MONGO_DB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
