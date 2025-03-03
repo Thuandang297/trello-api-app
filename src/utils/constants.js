d9a119e7059e97aa97ac7b24c332d4975306a804
Thuandang297 <Thuandang729@gamil.com>
2025-03-03 13:10:07 +0700

    again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WHITE_LIST_DOMAINS = ['https://trello-api-app.onrender.com', 'https://vite-trello-web-base-projec-git-163c32-thuans-projects-2d033766.vercel.app', 'https://vite-trello-web-base-projec-git-163c32-thuans-projects-2d033766.vercel.app']
+export const WHITE_LIST_DOMAINS = ['https://vite-trello-web-base-project-5fq6czy0v-thuans-projects-2d033766.vercel.app']
 // export const WHITE_LIST_DOMAINS = ['http://localhost:5173', 'http://localhost:5000']
 
 export const TYPE_BOARDS={
